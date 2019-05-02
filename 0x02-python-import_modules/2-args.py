53569715ed9190fc47672e011065a8337ba3c726
Arthur Damm <indigoarthur@gmail.com>
2019-05-02 20:41:32 +0000

    add 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    l = len(sys.argv) - 1
+    print("{} argument{}".format(l, "" if l == 1 else "s"))
+    for i in range(l):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
