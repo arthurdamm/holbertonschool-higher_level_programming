65907d545beaf0771d7faef9a865aadffc81d2e9
Arthur Damm <indigoarthur@gmail.com>
2019-05-07 20:47:12 +0000

    add 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     if not matrix:
         return None
     for submatrix in matrix:
-        for i in range(len(submatrix)):
-            print("{:d}".format(submatrix[i]),
-                  end="\n" if i is len(submatrix) - 1 else " ")
+        if submatrix:
+            for i in range(len(submatrix)):
+                print("{:d}".format(submatrix[i]),
+                      end="\n" if i is len(submatrix) - 1 else " ")
