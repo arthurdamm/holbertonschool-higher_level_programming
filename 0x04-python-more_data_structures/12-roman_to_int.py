e3c9b5c33c4a9bfb67c569b1b38b6bfc55b796fc
Arthur Damm <indigoarthur@gmail.com>
2019-05-10 00:33:24 +0000

    add 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) is not str:
-        return None
+    if not isinstance(roman_string, str):
+        return 0
     total = 0
     num = 0
     digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
