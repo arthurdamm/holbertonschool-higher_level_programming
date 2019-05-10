fe2fe9d594f26a47a11135e915b539ebc42f6c66
Arthur Damm <indigoarthur@gmail.com>
2019-05-10 00:37:40 +0000

    add 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,7 +6,10 @@ def roman_to_int(roman_string):
     num = 0
     digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     for r in reversed(roman_string):
-        num = digits[r]
+        if digits.get(r) is None:
+            num = 0
+        else:
+            num = digits[r]
         if total > num * 5:
             num = -num
         total += num
