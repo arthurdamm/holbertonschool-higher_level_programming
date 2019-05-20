4f3391c079f5d62698389110a61f082104c3897f
Arthur Damm <indigoarthur@gmail.com>
2019-05-20 17:09:53 +0000

    add 4-list_division.py

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    i = 0
+    ret = []
+    while i < list_length:
+        try:
+            res = my_list_1[i] / my_list_2[i]
+        except (ZeroDivisionError, TypeError, IndexError):
+            res = 0
+        ret.append(res)
+        i += 1
+    return ret
