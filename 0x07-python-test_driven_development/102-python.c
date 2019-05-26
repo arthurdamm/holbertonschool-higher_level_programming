7efc2d0ed0528f070b96e32be865b266f37886a4
Arthur Damm <indigoarthur@gmail.com>
2019-05-26 10:15:08 +0000

    add 102-python.c

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -11,26 +11,16 @@
  */
 void print_python_string(PyObject *p)
 {
-	wchar_t *wstr;
-
-	setlocale(LC_CTYPE,"en_US.UTF-8");
-    fflush(stdout);
-	printf("[.] string object info\n");
+	wprintf(L"[.] string object info\n");
 	if (strcmp(p->ob_type->tp_name, "str"))
 	{
-		printf("  [ERROR] Invalid String Object\n");
+		wprintf(L"  [ERROR] Invalid String Object\n");
 		return;
 	}
 	if (PyUnicode_IS_COMPACT_ASCII(p))
-		printf("  type: compact ascii\n");
+		wprintf(L"  type: compact ascii\n");
 	else
-		printf("  type: compact unicode object\n");
-	printf("  length: %lu\n", PyUnicode_GET_LENGTH(p));
-	printf("  value: ");
-	fflush(stdout);
-	wstr = PyUnicode_AS_UNICODE(p);
-	while (*wstr)
-		putwchar(*wstr++);
-	putchar('\n');
-	fflush(stdout);
+		wprintf(L"  type: compact unicode object\n");
+	wprintf(L"  length: %lu\n", PyUnicode_GET_LENGTH(p));
+	wprintf(L"  value: %ls\n", PyUnicode_AS_UNICODE(p));
 }
