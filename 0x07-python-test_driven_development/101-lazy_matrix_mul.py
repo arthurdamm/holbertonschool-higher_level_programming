07ea2e8597ab64228bd6b595fe33dfe5843d2cf2
Arthur Damm <indigoarthur@gmail.com>
2019-05-26 16:45:45 +0000

    add 101-lazy_matrix_mul.py

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -25,6 +25,7 @@ def lazy_matrix_mul(m_a, m_b):
         raise TypeError("m_a must be a list")
     if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
+    '''
     m_a_empty = False
     m_b_empty = False
     m_a_notrect = False
@@ -33,7 +34,7 @@ def lazy_matrix_mul(m_a, m_b):
     m_b_notnum = False
     for row in m_a:
         if not isinstance(row, list):
-            raise TypeError("m_a must be a list of lists")
+            pass #raise TypeError("m_a must be a list of lists")
         if len(row) != len(m_a[0]):
             m_a_notrect = True
         for num in row:
@@ -42,18 +43,20 @@ def lazy_matrix_mul(m_a, m_b):
 
     for row in m_b:
         if not isinstance(row, list):
-            raise TypeError("m_b must be a list of lists")
+            pass #raise TypeError("m_b must be a list of lists")
         if len(row) != len(m_b[0]):
             m_b_notrect = True
         for num in row:
             if not isinstance(num, (int, float)):
                 m_b_notnum = True
 
+    '''
     if len(m_a) == 0 or (len(m_a) == 1 and len(m_a[0]) == 0):
         raise ValueError("m_a can't be empty")
 
     if len(m_b) == 0 or (len(m_b) == 1 and len(m_b[0]) == 0)x:
         raise ValueError("m_b can't be empty")
+    '''
 
     if m_a_notnum:
         raise TypeError("m_a should contain only integers or floats")
@@ -67,6 +70,7 @@ def lazy_matrix_mul(m_a, m_b):
     if m_b_notrect:
         raise TypeError("each row of m_b must should be of the same size")
 
+    '''
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
 
