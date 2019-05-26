e7f5ebc91bc76e8f78dcade6ef638632656a18d5
Arthur Damm <indigoarthur@gmail.com>
2019-05-26 09:42:53 +0000

    add 102-python.c

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <wchar.h>
+#include <locale.h>
+#include <Python.h>
+#include <unicodeobject.h>
+
+/**
+ * print_python_sting - prints info about python strings
+ * @p: address of pyobject struct
+ */
+void print_python_string(PyObject *p)
+{
+	wchar_t *wstr;
+
+    fflush(stdout);
+ 	
+	printf("[.] string object info\n");
+	if (strcmp(p->ob_type->tp_name, "str"))
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
+	if (PyUnicode_IS_COMPACT_ASCII(p))
+		printf("  type: compact ascii\n");
+	else
+		printf("  type: compact unicode object\n");
+	printf("  length: %lu\n", PyUnicode_GET_LENGTH(p));
+	printf("  value: ");
+	fflush(stdout);
+	wstr = PyUnicode_AS_UNICODE(p);
+	while (*wstr)
+		putwchar(*wstr++);
+	putchar('\n');
+	fflush(stdout);
+}
