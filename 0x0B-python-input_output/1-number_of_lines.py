8267101ee703b8462398178bc784aac18512c901
Arthur Damm <indigoarthur@gmail.com>
2019-06-06 20:27:51 +0000

    add 1-number_of_lines.py

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -4,11 +4,9 @@
 
 def number_of_lines(filename=""):
     '''Method for reading from file.'''
-    num = 0
     with open(filename, "r") as f:
         lines = f.readlines()
         num = len(lines)
         if lines[-1][-1] != "\n":
             num -= 1
-    f.close()
-    return num
+        return num
