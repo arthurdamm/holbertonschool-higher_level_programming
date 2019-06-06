138aecc0e984541c2386e451b1fa8fabc0846642
Arthur Damm <indigoarthur@gmail.com>
2019-06-06 03:00:12 +0000

    add 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -8,4 +8,9 @@ class Square(Rectangle):
     def __init__(self, size):
         '''Constructor.'''
         self.integer_validator("size", size)
+        self.__size = size
         super().__init__(size, size)
+
+    def area(self):
+        '''Method for area of square.'''
+        return self.__size ** 2
