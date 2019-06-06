9d272c710724c840e15cda7dae1134310ec01a09
Arthur Damm <indigoarthur@gmail.com>
2019-06-06 23:39:03 +0000

    add 13-student.py

--- 0x0B-python-input_output/13-student.py
+++ 0x0B-python-input_output/13-student.py
@@ -15,7 +15,7 @@ class Student:
         if type(attrs) is list and all([type(x) == str for x in attrs]):
             return {k: v for k, v in self.__dict__.items() if k in attrs}
         else:
-            return self.__dict__
+            return self.__dict__.copy()
 
     def reload_from_json(self, json):
         '''Loads attributes from json.'''
