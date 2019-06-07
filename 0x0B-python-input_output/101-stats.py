c10c2a67d5d48d8e0703804f226e8339cdd9210b
Arthur Damm <arthurdamm@Arthurs-MacBook-Air.local>
2019-06-06 23:10:00 -0700

    add 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
 '''Module for log parsing script.'''
 import sys
-import re
 
 if __name__ == "__main__":
     size = [0]
     codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-    rex = re.compile(
-        '(\S+) - \[([^]]+)\] "GET /projects/260 HTTP/1.1" (\d+) (\d+)\n')
 
     def check_match(line):
         '''Checks for regexp match in line.'''
-        match = rex.search(line)
-        if not match:
-            return
         try:
-            size[0] += int(match.group(4))
-            code = int(match.group(3))
+            line = line[:-1]
+            words = line.split(" ")
+            size[0] += int(words[-1])
+            code = int(words[-2])
             if code in codes:
                 codes[code] += 1
         except:
