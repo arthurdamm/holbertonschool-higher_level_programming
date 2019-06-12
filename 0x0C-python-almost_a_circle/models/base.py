17c741d1f01f6c6e316dda7a82533ac9b0f4379b
Arthur Damm <arthurdamm@Arthurs-MacBook-Air.local>
2019-06-12 12:00:43 -0700

    add models/base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,7 +14,7 @@ class Base:
             self.id = id
         else:
             type(self).__nb_objects += 1
-            self.id = type(self).__nb_objects
+            self.id = Base.__nb_objects
 
     @staticmethod
     def to_json_string(list_dictionaries):
