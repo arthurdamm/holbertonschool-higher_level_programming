f6b6aecea4d8da2c61fbc1fdd0e5e3bc118d3f2a
Arthur Damm <arthurdamm@Arthurs-MacBook-Air.local>
2019-06-12 12:26:37 -0700

    add models/base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,6 +27,8 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         '''Unjsonifies a dictionary.'''
+        if json_string is None or not json_string
+            return []
         return loads(json_string)
 
     @classmethod
