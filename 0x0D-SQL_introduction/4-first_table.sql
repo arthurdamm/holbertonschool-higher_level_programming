ddd3006b5e54059032629bd9b9c362a1520af232
Arthur Damm <indigoarthur@gmail.com>
2019-07-15 21:17:43 +0000

    add 4-first_table.sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,7 +1,6 @@
 -- Creates first table
 -- Create first_table with id, name
 CREATE TABLE IF NOT EXISTS first_table (
-    id INT AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
+    id INT,
+    name VARCHAR(256)
 ) ENGINE=INNODB;
