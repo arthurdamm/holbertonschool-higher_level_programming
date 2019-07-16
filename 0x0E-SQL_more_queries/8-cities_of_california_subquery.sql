9d713ef45de0d4d9928ad713219312d4800ae5ff
Arthur Damm <indigoarthur@gmail.com>
2019-07-16 21:06:45 +0000

    add 8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- Cities of CA
 -- select cities where state_id corresponds to name "California"
-SELECT * FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
