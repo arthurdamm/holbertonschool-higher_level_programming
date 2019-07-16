e95e9dc295748b4d8355c12ece94b2a5a8cdb9ab
Arthur Damm <indigoarthur@gmail.com>
2019-07-16 22:57:54 +0000

    add 12-no_genre.sql

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- Genre ID by show
+-- select and join
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
