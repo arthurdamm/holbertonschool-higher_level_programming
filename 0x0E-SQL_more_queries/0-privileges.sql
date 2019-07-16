16498d0e82604b8f30b671095bb4d1ddead78c8a
Arthur Damm <indigoarthur@gmail.com>
2019-07-16 14:28:01 +0000

    add 0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- My privileges
 -- show privs for user1
-SHOW GRANTS FOR user_0d_1;
+SHOW GRANTS FOR user_0d_1@localhost;
 -- show privs for user2
-SHOW GRANTS FOR user_0d_2;
+SHOW GRANTS FOR user_0d_2@localhost;
