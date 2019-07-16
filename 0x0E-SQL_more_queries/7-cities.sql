265d0a5c713d7ff9bf49e86f6876ddc613ebe1fa
Arthur Damm <indigoarthur@gmail.com>
2019-07-16 21:41:15 +0000

    add 7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,5 +6,5 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES states(id)
+    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
 ) ENGINE=INNODB;
