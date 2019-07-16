85bbb6df09a4fa6ce738382d40fc4dc45081824c
Arthur Damm <indigoarthur@gmail.com>
2019-07-16 20:55:51 +0000

    add 7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,10 @@
 -- States table
--- create db
+-- create a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
--- create table
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+-- use the new db
+USE hbtn_0d_usa;
+-- create the table now
+CREATE TABLE IF NOT EXISTS cities (
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
