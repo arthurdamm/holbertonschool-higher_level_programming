0591ee209b0bdd1379f3b30ff2dff0bdc4278218
Arthur Damm <indigoarthur@gmail.com>
2019-07-16 22:38:43 +0000

    add 11-genre_id_all_shows.sql

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- Genre ID by show
+-- select and join
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
