67ea5b14d00c85a8950889adbc65756e9b4beb6c
Arthur Damm <indigoarthur@gmail.com>
2019-07-17 05:18:39 +0000

    add 102-rating_shows.sql

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- Rotten tomatoes
 -- select sum of ratings
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) as "rating" FROM tv_shows
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS "rating" FROM tv_shows
 LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
 GROUP BY tv_shows.title
 ORDER BY SUM(tv_show_ratings.rate) DESC
