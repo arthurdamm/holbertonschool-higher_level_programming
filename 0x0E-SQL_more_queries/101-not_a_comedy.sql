965d15f9c607f82f132d5d41ad4b201ddcdcaf47
Arthur Damm <indigoarthur@gmail.com>
2019-07-17 04:53:54 +0000

    add 101-not_a_comedy.sql

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,9 @@
+-- NOT Only comedy
+-- select show names that match genre comedy
+SELECT title from tv_shows WHERE title NOT IN (
+SELECT s.title FROM tv_genres g, tv_show_genres t, tv_shows s
+WHERE g.id = t.genre_id
+    AND t.show_id = s.id
+    AND g.name = "Comedy"
+) ORDER BY title ASC;
+; 
