c9331ba506dbc0153424c444c81f14f27e50d9e5
Arthur Damm <indigoarthur@gmail.com>
2019-07-17 03:05:09 +0000

    add 14-my_genres.sql

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+-- My genres
+-- select with many conditions
+SELECT g.name FROM tv_genres g, tv_show_genres t, tv_shows s
+WHERE g.id = t.genre_id
+    AND t.show_id = s.id
+    AND s.title = "Dexter"
+; 
