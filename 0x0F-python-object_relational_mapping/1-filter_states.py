ef17197a736480eaae7d242e1104eeaf58de6c53
Arthur Damm <indigoarthur@gmail.com>
2019-08-10 19:29:02 +0000

    Try try & except

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,14 +5,20 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
-                           user=argv[1], passwd=argv[2], db=argv[3])
-    cur = conn.cursor()
-    cur.execute("""
-SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY states.id ASC
-""")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
+    try:
+        conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                               user=argv[1], passwd=argv[2], db=argv[3])
+        cur = conn.cursor()
+        cur.execute("""
+    SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY states.id ASC
+    """)
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+    except:
+        Pass
+    try:
+        cur.close()
+        conn.close()
+    except:
+        Pass
