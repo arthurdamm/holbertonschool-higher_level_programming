6a874772f2a7e1d4ff2c5dc72c83c2ba1e221186
Arthur Damm <indigoarthur@gmail.com>
2019-08-10 18:57:20 +0000

    add 10-model_state_my_get.py

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Return State object matching arg"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'
+            .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+states = session.query(State).\
+            filter(State.name == argv[4]).order_by(State.id).all()
+if states:
+    print("{}".format(states[0].id))
+else:
+    print("Not found")
+session.close()
