56449bf09961393e2806d4fe70eef2c718cc523f
Arthur Damm <indigoarthur@gmail.com>
2019-08-10 19:24:16 +0000

    Fix format

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                            user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
     cur.execute("""
-SELECT * FROM states WHERE states.name LIKE 'X%' ORDER BY states.id ASC
+SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY states.id ASC
 """)
     query_rows = cur.fetchall()
     for row in query_rows:
