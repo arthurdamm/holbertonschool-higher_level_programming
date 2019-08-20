8d838a149967473e70aa4359dc24b22315915352
Arthur Damm <indigoarthur@gmail.com>
2019-08-20 20:04:41 +0000

    add 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Finds peak in unsorted list of integers"""
 
+
 def find_peak(list_of_integers):
     """Finds peak in unsorted list of integers"""
     for i, n in enumerate(list_of_integers):
