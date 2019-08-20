d71473646decf8b092f8da2fa70883eb8061c1d9
Arthur Damm <indigoarthur@gmail.com>
2019-08-20 20:50:01 +0000

    add 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,8 +4,23 @@
 
 def find_peak(list_of_integers):
     """Finds peak in unsorted list of integers"""
-    for i, n in enumerate(list_of_integers):
-        if i < len(list_of_integers) - 1 and n < list_of_integers[i + 1]:
-            continue
-        return n
-    return None
+
+    if len(list_of_integers) > 1:
+        if list_of_integers[0] >= list_of_integers[1]:
+            return list_of_integers[0]
+        if list_of_integers[-1] >= list_of_integers[-2]:
+            return list_of_integers[-1]
+        return _find_peak(list_of_integers, 0, len(list_of_integers))
+    if not list_of_integers:
+        return None
+    return list_of_integers[0]
+
+
+def _find_peak(lint, start, stop):
+    """Recursively finds peak"""
+    if stop - start < 2:
+        return None
+    mid = (start + stop) // 2
+    if lint[mid] >= lint[mid - 1] and lint[mid] >= lint[mid + 1]:
+        return lint[mid]
+    return _find_peak(lint, start, mid) or _find_peak(lint, mid, stop)
