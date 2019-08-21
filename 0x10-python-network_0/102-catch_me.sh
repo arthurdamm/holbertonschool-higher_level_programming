d3b3a6d24fa213da377196f8c2e72f8768ee731c
Arthur Damm <indigoarthur@gmail.com>
2019-08-21 01:40:10 +0000

    add 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # catch as catch can
-curl -sL -X PUT -H "Origin:HolbertonSchool" -d "user_id=98" "$1"
+curl -sL -X PUT -H "Origin:HolbertonSchool" -d "user_id=98" "0.0.0.0:5000/catch_me"
