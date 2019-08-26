a45d159581d4f3c1838dca372e263e569e41ca1e
Arthur Damm <indigoarthur@gmail.com>
2019-08-26 21:08:20 +0000

    add 2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Fetches header from url passed as arg with email data"""
+from urllib import request, parse
+import sys
+
+if __name__ == "__main__":
+    data = parse.urlencode({"email": sys.argv[2]}).encode()
+        with request.urlopen(sys.argv[1], data) as response:
+                print(response.read().decode('utf-8'))
