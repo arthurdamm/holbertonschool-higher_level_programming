3a3076e5116f3b580f986357b6e5b3381fea1184
Arthur Damm <indigoarthur@gmail.com>
2019-08-26 21:09:14 +0000

    add 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,5 +5,5 @@ import sys
 
 if __name__ == "__main__":
     data = parse.urlencode({"email": sys.argv[2]}).encode()
-        with request.urlopen(sys.argv[1], data) as response:
-                print(response.read().decode('utf-8'))
+    with request.urlopen(sys.argv[1], data) as response:
+        print(response.read().decode('utf-8'))
