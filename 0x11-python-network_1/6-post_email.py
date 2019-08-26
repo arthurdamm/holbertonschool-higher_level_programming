334940272220fb2976ebd109f5e952ffb60be320
Arthur Damm <indigoarthur@gmail.com>
2019-08-26 21:55:16 +0000

    add 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,5 +4,5 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    response = requests.get(argv[1], data={'email': argv[2]})
+    response = requests.post(argv[1], data={'email': argv[2]})
     print(response.text)
