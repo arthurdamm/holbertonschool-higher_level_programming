30d981b0a0a9f32a2698f64719434eaae69e89a3
Arthur Damm <indigoarthur@gmail.com>
2019-08-26 23:57:15 +0000

    add 8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Uses requests module. Prints error code"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    data = {"q": argv[1][0] if len(argv) > 1 else ""}
+    response = requests.post(url, data=data)
+    try:
+        d = response.json()
+        if not d:
+            raise ValueError
+        print(d)
+    except ValueError:
+        print("No result")
