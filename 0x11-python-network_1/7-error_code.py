361c04b25aa1ed40511a181b7c1636527915fb82
Arthur Damm <indigoarthur@gmail.com>
2019-08-26 22:06:47 +0000

    add 7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Uses requests module. Prints error code"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    response = requests.get(argv[1])
+    if response.status_code > 400:
+        print("Error code:", response.status_code)
+    else:
+        print(response.text)
