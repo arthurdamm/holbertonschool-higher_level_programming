79c890fdd28eef19c5f2f8cc9324c07399bbeb75
Arthur Damm <indigoarthur@gmail.com>
2019-08-27 01:29:59 +0000

    add 100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Github code challenge"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits"\
+          .format(argv[2], argv[1])
+    r = requests.get(url)
+    for i in r.json():
+        print(i.get("sha"), i.get("commit").get("author").get("name"))
