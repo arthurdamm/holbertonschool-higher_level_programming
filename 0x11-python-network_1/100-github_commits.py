01cd9c07cafc1e8097045f230cf5b8f8abc59af9
Arthur Damm <indigoarthur@gmail.com>
2019-08-27 01:31:32 +0000

    add 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,5 +7,8 @@ if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits"\
           .format(argv[2], argv[1])
     r = requests.get(url)
+    n = 0
     for i in r.json():
-        print(i.get("sha"), i.get("commit").get("author").get("name"))
+        if n < 10:
+            print(i.get("sha"), i.get("commit").get("author").get("name"))
+        n += 1
