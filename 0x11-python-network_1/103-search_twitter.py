081db90d9c50a654a864f4b79fc3dc242a504f01
Arthur Damm <indigoarthur@gmail.com>
2019-08-27 06:47:38 +0000

    add 103-search_twitter.py

--- 0x11-python-network_1/103-search_twitter.py
+++ 0x11-python-network_1/103-search_twitter.py
@@ -7,36 +7,41 @@ import base64
 
 def doit(apikey, secretkey, search):
     """Connects to Twitter, authenticates, and searches"""
-    key = "{}:{}".format(apikey, secretkey).encode('ascii')
-    key = base64.b64encode(key).decode('ascii')
-    auth_headers = {
-        'Authorization': 'Basic ' + key,
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-    }
-    auth_data = {
-        'grant_type': 'client_credentials'
-    }
-    r = requests.post('https://api.twitter.com/oauth2/token',
-                      headers=auth_headers, data=auth_data)
-    token = r.json().get('access_token')
+    try:
+        key = "{}:{}".format(apikey, secretkey).encode('ascii')
+        key = base64.b64encode(key).decode('ascii')
+        auth_headers = {
+            'Authorization': 'Basic ' + key,
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+        }
+        auth_data = {
+            'grant_type': 'client_credentials'
+        }
+        r = requests.post('https://api.twitter.com/oauth2/token',
+                          headers=auth_headers, data=auth_data)
+        token = r.json().get('access_token')
 
-    search_headers = {
-        'Authorization': 'Bearer ' + token
-    }
-    search_data = {
-        'q': search,
-        'count': 5
-    }
-    r = requests.get('https://api.twitter.com/1.1/search/tweets.json',
-                     headers=search_headers, params=search_data)
+        search_headers = {
+            'Authorization': 'Bearer ' + token
+        }
+        search_data = {
+            'q': search,
+            'count': 5
+        }
+        r = requests.get('https://api.twitter.com/1.1/search/tweets.json',
+                         headers=search_headers, params=search_data)
 
-    if not r.json().get('statuses'):
-        return
+        if not r.json().get('statuses'):
+            return
 
-    for tweet in r.json().get('statuses'):
-        print("[{}] {} by {}".format(tweet.get('id'), tweet.get('text'),
-                                     tweet.get('user').get('name')))
-    return r
-
+        for tweet in r.json().get('statuses'):
+            try:
+                print("[{}] {} by {}".format(tweet.get('id'), tweet.get('text'),
+                                             tweet.get('user').get('name')))
+            except Exception:
+                pass
+        return r
+    except Exception:
+        return None
 if __name__ == "__main__":
     doit(argv[1], argv[2], argv[3])
