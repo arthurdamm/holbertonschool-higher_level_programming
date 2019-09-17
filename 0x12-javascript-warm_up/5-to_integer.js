2f58893ab62b20dedb23066f0900043c48d2ccc7
arthurdamm <indigoarthur@gmail.com>
2019-09-17 20:02:29 +0000

    add 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-n = parseInt(process.argv[2])
-if (n !== NaN)
-  console.log('My number: ' + n)
-else
-  console.log('Not a number')
+let n = parseInt(process.argv[2]);
+if (!isNaN(n)) {
+  console.log('My number: ' + n);
+} else {
+  console.log('Not a number');
+}
