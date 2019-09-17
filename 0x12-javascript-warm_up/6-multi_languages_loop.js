7e40003a5ff229e92d21522a6a90b0e382e52c36
Arthur Damm <indigoarthur@gmail.com>
2019-09-17 20:55:17 +0000

    add 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < lines.length; i++) {
-  console.log(lines[i]);
+const line = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < line.length; i++) {
+  console.log(line[i]);
 }
