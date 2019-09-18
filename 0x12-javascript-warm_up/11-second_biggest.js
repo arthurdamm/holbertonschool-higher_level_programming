7834bc6f3c4017df0c0b605a88184782dca3de26
Arthur Damm <indigoarthur@gmail.com>
2019-09-18 04:35:11 +0000

    add 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,2 @@
 #!/usr/bin/node
-if (process.argv.length < 4) {
-  console.log(0);
-} else {
-  console.log(process.argv.slice(2).sort((a, b) => b - a)[1]);
-}
+console.log(process.argv.length < 4 ? 0 : process.argv.slice(2).sort((a, b) => b - a)[1]);
