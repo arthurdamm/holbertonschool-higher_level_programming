52a61c5aca7eb11d3b33d69a93d4258fb9c278e3
Arthur Damm <indigoarthur@gmail.com>
2019-09-18 02:09:07 +0000

    add 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const n = parseInt(process.argv[2]);
 if (!isNaN(n)) {
-    console.log(('X'.repeat(n) + '\n').repeat(n));
+  console.log(('X'.repeat(n) + '\n').repeat(n));
 } else {
   console.log('Missing size');
 }
