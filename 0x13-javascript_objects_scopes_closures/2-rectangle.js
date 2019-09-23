51012afe96bdfc5d3be096565c4e8c83a57a5588
Arthur Damm <indigoarthur@gmail.com>
2019-09-23 18:50:36 +0000

    add 2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,8 +2,8 @@
 // Checked Rectangle Class
 class Rectangle {
   constructor (w, h) {
-    if ((w = parseInt(w)) && w > 0 &&
-        (h = parseInt(h)) && h > 0) {
+    if ((w = parseInt(w)) > 0 &&
+        (h = parseInt(h)) > 0) {
       this.width = w;
       this.height = h;
     }
