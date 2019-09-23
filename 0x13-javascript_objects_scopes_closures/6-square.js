2409d5f157d6405ffbe7df317ec7d670d2014d50
Arthur Damm <indigoarthur@gmail.com>
2019-09-23 19:25:51 +0000

    add 6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// Square class extends square
+const SquareBase = require('./5-square');
+
+class Square extends SquareBase {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    process.stdout.write((c.repeat(this.width) + '\n').repeat(this.height));
+  }
+}
+
+module.exports = Square;
