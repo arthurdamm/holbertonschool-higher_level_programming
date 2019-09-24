4e7a96e52e6fadd76f48be8e547fcb7da99119c0
Arthur Damm <indigoarthur@gmail.com>
2019-09-24 21:10:42 +0000

    add 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 // makes get request for SW movie id
 const request = require('request');
-const find = '/people/18/';
+const find = '/18/';
 request(process.argv[2], function (error, response, body) {
-  try {
-    !error && body && console.log(JSON.parse(body).results.filter(film => film.characters.includes(find)).length);
-  } catch (error) {
+  !error && body && console.log(JSON.parse(body).results.filter(film => film.characters.includes(find)).length);
   }
 });
