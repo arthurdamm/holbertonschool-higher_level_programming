351d8d4d6e6cbe51116a254ad26a2d54b378c232
Arthur Damm <indigoarthur@gmail.com>
2019-09-24 19:03:59 +0000

    add 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,6 +2,5 @@
 // reads file passed as arg
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', (error, data) => {
-  if (error) console.log(error.stack);
   console.log(error || data);
 });
