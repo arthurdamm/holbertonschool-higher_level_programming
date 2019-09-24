eab33b24d63a34ee32b9b3ad30a6a93a9b4d35ad
Arthur Damm <indigoarthur@gmail.com>
2019-09-24 19:23:03 +0000

    add 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // makes get request for SW movie id
 const request = require('request');
-const find = '/18/';
+const find = '/people/18/';
 request(process.argv[2], function (error, response, body) {
   try {
     !error && body && console.log(JSON.parse(body).results.filter(film => film.characters.includes(find)).length);
