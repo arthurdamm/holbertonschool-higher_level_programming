26e02f8f29bb266e014e13bddb47db4d632110a3
Arthur Damm <indigoarthur@gmail.com>
2019-09-24 19:17:01 +0000

    add 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,6 +3,5 @@
 const request = require('request');
 const find = 'https://swapi.co/api/people/18/';
 request(process.argv[2], function (error, response, body) {
-  error && console.log(error);
-  console.log(JSON.parse(body).results.filter(film => film.characters.includes(find)).length);
+  !error && body && console.log(JSON.parse(body).results.filter(film => film.characters.includes(find)).length);
 });
