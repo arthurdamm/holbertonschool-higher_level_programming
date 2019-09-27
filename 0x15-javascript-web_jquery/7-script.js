adbcb99d4b9ebe289a032c3a34878356471067a4
Arthur Damm <indigoarthur@gmail.com>
2019-09-27 02:04:21 +0000

    add 7-script.js

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,2 +1,2 @@
-$.get('https://swapi.co/api/people/5/?format=json', data =>
-  $('DIV#character').text(data.name));
+$.get('https://swapi.co/api/people/5/?format=json',
+  data => $('DIV#character').text(data.name));
