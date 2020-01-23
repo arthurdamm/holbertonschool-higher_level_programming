dfc2a828133dc6c535adc14f4871a6d12d5442c9
Arthur Damm <indigoarthur@gmail.com>
2020-01-22 21:01:40 -0800

    add 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -8,17 +8,15 @@ codes = ['200', '301', '400', '401',
 code_hist = []
 total_size = 0
 try:
-    while True:
-        for cnt, line in enumerate(sys.stdin):
-            tokens = line.split(' ')
-            total_size += int(tokens[-1])
-            code_hist.append(tokens[-2])
-            if cnt == 9:
-                print('File size: ' + str(total_size))
-                for code in codes:
-                    if code in code_hist:
-                        print('{}: {}'.format(code, code_hist.count(code)))
-                break
+    for cnt, line in enumerate(sys.stdin):
+        tokens = line.split(' ')
+        total_size += int(tokens[-1])
+        code_hist.append(tokens[-2])
+        if cnt == 9:
+            print('File size: ' + str(total_size))
+            for code in codes:
+                if code in code_hist:
+                    print('{}: {}'.format(code, code_hist.count(code)))
 except KeyboardInterrupt:
     print('File size: ' + str(total_size))
     for code in codes:
