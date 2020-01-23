68512360b6ed88e83e655404fdf3236e979fca78
Arthur Damm <indigoarthur@gmail.com>
2020-01-22 21:15:58 -0800

    add 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,25 +1,37 @@
 #!/usr/bin/python3
-"""This is just a document string"""
-
-
+'''Module for log parsing script.'''
 import sys
-codes = ['200', '301', '400', '401',
-         '403', '404', '405', '500']
-code_hist = []
-total_size = 0
-try:
-    for cnt, line in enumerate(sys.stdin):
-        tokens = line.split(' ')
-        total_size += int(tokens[-1])
-        code_hist.append(tokens[-2])
-        if cnt == 9:
-            print('File size: ' + str(total_size))
-            for code in codes:
-                if code in code_hist:
-                    print('{}: {}'.format(code, code_hist.count(code)))
-except KeyboardInterrupt:
-    print('File size: ' + str(total_size))
-    for code in codes:
-        if code in code_hist:
-            print('{}: {}'.format(code, code_hist.count(code)))
-    sys.exit()
+
+if __name__ == "__main__":
+    size = [0]
+    codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+
+    def check_match(line):
+        '''Checks for regexp match in line.'''
+        try:
+            line = line[:-1]
+            words = line.split(" ")
+            size[0] += int(words[-1])
+            code = int(words[-2])
+            if code in codes:
+                codes[code] += 1
+        except:
+            pass
+
+    def print_stats():
+        '''Prints accumulated statistics.'''
+        print("File size: {}".format(size[0]))
+        for k in sorted(codes.keys()):
+            if codes[k]:
+                print("{}: {}".format(k, codes[k]))
+    i = 1
+    try:
+        for line in sys.stdin:
+            check_match(line)
+            if i % 10 == 0:
+                print_stats()
+            i += 1
+    except KeyboardInterrupt:
+        print_stats()
+        raise
+    print_stats()
